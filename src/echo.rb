dbc7d4d80ff3890ee98bdfb50a5beb28a0f65c4d
Tanja Alva <tanja.alva@gmail.com>
2023-02-11 10:23:33 +0800

    Turned into a class

--- src/echo.rb
+++ src/echo.rb
@@ -1,17 +1,26 @@
 require 'socket'
 
-server = TCPServer.new ENV.fetch('ECHO_HOST', 'localhost'), ENV.fetch('ECHO_PORT', 4242)
-puts "Listening on %s\n" % server.addr.join(":")
+class Echo
+  def initialize(host: '127.0.0.1', port: 4242)
+    @host = host
+    @port = port
+  end
 
-begin
-  loop do
-    Thread.start server.accept do |client|
-      print client
-      client.puts "hey"
-      client.puts "Time is #{Time.now}"
-      client.close
+  def start
+    server = TCPServer.new @host, @port
+    puts "Listening on %s:%s" % [server.addr[2], server.addr[1]]
+    begin
+      loop do
+        Thread.start server.accept do |client|
+          client.print "HTTP/1.1 200\r\n"
+          client.print "Content-Type: text/html\r\n"
+          client.print "\r\n"
+          client.print "Hello world! The time is #{Time.now}"
+          client.close
+        end
+      end
+    rescue Exception => e
+      puts "Stopped"
     end
   end
-rescue Exception => e
-  puts "Stopped"
 end
